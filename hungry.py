1e09a36874011f870e65690329529aed4ec6e025
Jatin Patel <jatin212001@gmail.com>
2026-01-30 21:13:04 +0530

    Implement logic

--- hungry.py
+++ hungry.py
@@ -1,1 +1,5 @@
-print("I'm hungry!  Feed me with anything !")
+hungry = input("Are you hungry? (yes/no): " ).strip().lower()
+if hungry == "yes":
+    print("Let's get some food!")   
+elif hungry == "no":
+    print("Great! Let's continue working.")
