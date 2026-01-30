10d67652570cf026c0aa12411d6b1f22afe07983
Jatin Patel <jatin212001@gmail.com>
2026-01-30 21:32:19 +0530

    Revert "Implement logic"
    This reverts commit c88ecb83dcbf76619ef7930c52779a66bcfdb62f.

--- hungry.py
+++ hungry.py
@@ -1,9 +1,5 @@
 hungry = input("Are you hungry? (yes/no): " ).strip().lower()
 if hungry == "yes":
     print("Let's get some food!")   
-    print("Here are some options:")
-    print("1. Pizza")
-    print("2. Salad")
-    print("3. Sandwich")
 elif hungry == "no":
     print("Great! Let's continue working.")
