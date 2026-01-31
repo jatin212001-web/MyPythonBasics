ec297afbcfb8437dec302983f2717418be5c5383
Jatin Patel <jatin212001@gmail.com>
2026-01-31 19:49:27 +0530

    added new changes in new branch

--- hungry.py
+++ hungry.py
@@ -3,3 +3,11 @@ if hungry == "yes":
     print("Let's get some food!")   
 elif hungry == "no":
     print("Great! Let's continue working.")
+else:
+    print("Please answer with 'yes' or 'no'.")
+
+print("Thank you for your response.")
+
+
+
+#retun number with two two decimal numbers from the user
